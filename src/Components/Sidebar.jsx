9da4fd100057092e40311544bf8657c0b9fd980a
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-08-04 11:30:29 +0300

    add sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,13 +1,79 @@
-import { Box } from '@mui/material'
+import { AccountBox, Article, Home, ModeNight, People, Person, Settings, Store } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2} sx={{display: {
+    <Box flex={1} p={2} sx={{display: {
         xs: "none",
         sm: "block"
     }}}>
-      sidebar
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <Home/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <Article/>
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <People/>
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <Store/>
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <Person/>
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <Settings/>
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <AccountBox/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#" >
+              <ListItemIcon>
+                <ModeNight/>
+              </ListItemIcon>
+              <Switch/>
+            </ListItemButton>
+          </ListItem>
+        </List>
     </Box>
   )
 }
